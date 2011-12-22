20c55a1751a04e6546b296f2f86946f52812f972
Ralph Bean <ralph.bean@gmail.com>
2011-12-21 22:58:01 -0500

    Cards now load tags as per the spec.

--- fedoratagger/widgets/card.py
+++ fedoratagger/widgets/card.py
@@ -1,8 +1,20 @@
 import tw2.core
 import tw2.forms
+import random
 
 import fedoratagger.model as m
 from fedoratagger.widgets.voting import TagWidget
+
+def pick(tags):
+    """ Select a subset of tags 'smartly'.  See the README. """
+
+    if len(tags) <= 5:
+        return sorted(tags, m.packages.tag_sorter, reverse=True)
+
+    # Three most popular and two random others
+    tags = sorted(tags, m.packages.tag_sorter, reverse=True)
+    return tags[:3] + random.sample(tags[3:], 2)
+
 
 class CardWidget(tw2.forms.LabelField):
     """ Tiny Voting Widget """
@@ -15,5 +27,5 @@ class CardWidget(tw2.forms.LabelField):
 
     def prepare(self):
         super(CardWidget, self).prepare()
-        self.tags = [TagWidget(tag=tag) for tag in self.package.tags]
+        self.tags = [TagWidget(tag=tag) for tag in pick(self.package.tags)]
         self.tags[0].css_class += " selected"
