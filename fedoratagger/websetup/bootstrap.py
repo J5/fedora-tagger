204edc72e0f7d15d0de940a9664f71a95bee232f
Ralph Bean <ralph.bean@gmail.com>
2011-12-21 23:21:27 -0500

    Method for downloading package icons.

--- fedoratagger/websetup/bootstrap.py
+++ fedoratagger/websetup/bootstrap.py
@@ -11,8 +11,31 @@ import urllib
 import sqlite3
 import os
 import sys
+import shutil
+
+
+def get_icons():
+    print "Getting icons."
+    root = os.path.sep.join(__file__.split(os.path.sep)[:-3])
+    cwd = os.getcwd()
+
+    if os.path.exists(cwd + "/icons.tar.xz"):
+        print "Icons have already been downloaded."
+    else:
+        url = "http://johnp.fedorapeople.org/downloads/xapian/icons.tar.xz"
+        print "Getting", url
+        urllib.urlretrieve(url, cwd + "/icons.tar.xz")
+
+    status, output = commands.getstatusoutput('tar xvf %s/icons.tar.xz' % cwd)
+    if status != 0:
+        raise Exception("Failed to decompress icons.")
+
+    shutil.rmtree(root + "/fedoratagger/public/images/icons", True)
+    shutil.move(cwd + '/icons', root + '/fedoratagger/public/images')
+
 
 def import_pkgdb_tags():
+    print "Importing pkgdb tags."
     repo = "F-16-i386-u"
     base_url = "https://admin.fedoraproject.org/pkgdb"
     url = base_url + "/lists/sqlitebuildtags/F-16-i386-u"
@@ -64,43 +87,11 @@ def bootstrap(command, conf, vars):
     # <websetup.bootstrap.before.auth
     from sqlalchemy.exc import IntegrityError
     try:
-        u = model.User()
-        u.user_name = u'manager'
-        u.display_name = u'Example manager'
-        u.email_address = u'manager@somedomain.com'
-        u.password = u'managepass'
-    
-        model.DBSession.add(u)
-    
-        g = model.Group()
-        g.group_name = u'managers'
-        g.display_name = u'Managers Group'
-    
-        g.users.append(u)
-    
-        model.DBSession.add(g)
-    
-        p = model.Permission()
-        p.permission_name = u'manage'
-        p.description = u'This permission give an administrative right to the bearer'
-        p.groups.append(g)
-    
-        model.DBSession.add(p)
-    
-        u1 = model.User()
-        u1.user_name = u'editor'
-        u1.display_name = u'Example editor'
-        u1.email_address = u'editor@somedomain.com'
-        u1.password = u'editpass'
-    
-        model.DBSession.add(u1)
-        model.DBSession.flush()
-
+        get_icons()
         import_pkgdb_tags()
 
         transaction.commit()
     except IntegrityError:
-        print 'Warning, there was a problem adding your auth data, it may have already been added:'
         import traceback
         print traceback.format_exc()
         transaction.abort()
