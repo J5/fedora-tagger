8366a253180683e5df7ee8f1442c2dc51fe4f71e
Ralph Bean <ralph.bean@gmail.com>
2011-12-22 13:26:57 -0500

    Cards remember your votes now.

--- fedoratagger/widgets/templates/tag.mak
+++ fedoratagger/widgets/templates/tag.mak
@@ -1,8 +1,8 @@
 <li id="tag-${str(w.tag.id)}" class="${w.css_class}">
 <div class="voter">
-  <div class="arrow up" onclick="upvote(${str(w.tag.id)});"></div>
-  <div class="total">${str(w.tag.total)}</div>
-  <div class="arrow down" onclick="downvote(${str(w.tag.id)});"></div>
+  <div class="arrow up${w.upcls}" onclick="upvote(${str(w.tag.id)});"></div>
+  <div class="total ${w.textcls}">${str(w.tag.total)}</div>
+  <div class="arrow down${w.downcls}" onclick="downvote(${str(w.tag.id)});"></div>
 </div>
 <a href="#">${w.tag.label.label}</a>
 </li>
--- fedoratagger/widgets/voting.py
+++ fedoratagger/widgets/voting.py
@@ -13,3 +13,39 @@ class TagWidget(tw2.forms.LabelField):
     resources = [voting_js] + tw2.jqplugins.gritter.gritter_resources
     tag = tw2.core.Param()
     template = 'fedoratagger.widgets.templates.tag'
+
+    @property
+    def upcls(self):
+        user = m.get_user()
+        query = m.Vote.query.filter_by(user=user, tag=self.tag)
+        if query.count() == 0:
+            return ""
+
+        if not query.one().like:
+            return ""
+
+        return "mod"
+
+    @property
+    def downcls(self):
+        user = m.get_user()
+        query = m.Vote.query.filter_by(user=user, tag=self.tag)
+        if query.count() == 0:
+            return ""
+
+        if query.one().like:
+            return ""
+
+        return "mod"
+
+    @property
+    def textcls(self):
+        user = m.get_user()
+        query = m.Vote.query.filter_by(user=user, tag=self.tag)
+        if query.count() == 0:
+            return ""
+
+        if query.one().like:
+            return "up_text"
+
+        return "down_text"
