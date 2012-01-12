04fc007ee4c8ecf006cc7bf1e825d94f33d39789
Ralph Bean <ralph.bean@gmail.com>
2012-01-12 13:11:28 -0500

    Keep deep-linking hashtags out of the _csrf_token

--- fedoratagger/public/javascript/query.js
+++ fedoratagger/public/javascript/query.js
@@ -6,7 +6,7 @@ $.extend({
         {
             hash = hashes[i].split('=');
             vars.push(hash[0]);
-            vars[hash[0]] = hash[1];
+            vars[hash[0]] = hash[1].split('#')[0];
         }
         return vars;
     },
