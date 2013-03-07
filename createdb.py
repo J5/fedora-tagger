3b7b872dc7e71e96061f036a4bfc3ca6a0d30759
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-03-07 20:49:35 +0100

    Add the __requires__ line to make it work on EL6

--- createdb.py
+++ createdb.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
+__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
+import pkg_resources
+
 from taggerapi import APP
 from taggerapi.taggerlib import model
 
-model.create_tables(APP.config['DB_URL'], True)
+model.create_tables(APP.config['DB_URL'], debug=True)
