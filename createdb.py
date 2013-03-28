db057a87e288c4320bec3bc12fc32513e6d25cac
Ralph Bean <rbean@redhat.com>
2013-03-28 13:09:03 -0400

    Renamespace the createdb script.

--- createdb.py
+++ createdb.py
@@ -3,7 +3,7 @@
 __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
 import pkg_resources
 
-from taggerapi import APP
-from taggerapi.taggerlib import model
+from fedoratagger import APP
+from fedoratagger.lib import model
 
 model.create_tables(APP.config['DB_URL'], debug=True)
