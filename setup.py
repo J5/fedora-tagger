771707ce3bf753dac489ff5bf0fd4a3babac6f38
Ralph Bean <rbean@redhat.com>
2013-04-05 10:15:39 -0400

    A setup.py for egg info entrypoints stuff mostly.

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+Setup script
+"""
+
+from setuptools import setup
+
+try:
+    import logging
+    import multiprocessing
+except ImportError:
+    pass
+
+
+def get_requires(filename="requirements.txt"):
+    with open(filename, 'r') as f:
+        return f.readlines()
+
+setup(
+    name='fedoratagger',
+    description='A web application for tagging and ranking packages',
+    version='2.0.0a',
+    author='Pierre-Yves Chibon and Ralph Bean',
+    author_email='pingou@pingoured.fr, ralph@fedoraproject.org',
+    license='GPLv2+',
+    url='https://github.com/fedora-infra/fedora-tagger/',
+    packages=['fedoratagger'],
+    include_package_data=True,
+    install_requires=get_requires(),
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+    ],
+    entry_points='''
+    [tw2.widgets]
+    widgets = fedoratagger.frontend.widgets
+    '''
+)
