c76d1301b73eb0662873055db4e8600ef160f83e
Ralph Bean <rbean@redhat.com>
2013-04-08 16:27:49 -0400

    New test-requirements.txt file.

--- setup.py
+++ setup.py
@@ -16,6 +16,10 @@ def get_requires(filename="requirements.txt"):
     with open(filename, 'r') as f:
         return f.readlines()
 
+def get_tests_require(filename="test-requirements.txt"):
+    with open(filename, 'r') as f:
+        return f.readlines()
+
 setup(
     name='fedoratagger',
     description='A web application for tagging and ranking packages',
@@ -28,9 +32,7 @@ setup(
     include_package_data=True,
     install_requires=get_requires(),
     test_suite='nose.collector',
-    tests_require=[
-        'nose',
-    ],
+    tests_require=get_tests_require(),
     entry_points='''
     [tw2.widgets]
     widgets = fedoratagger.frontend.widgets
